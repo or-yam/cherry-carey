cb9d1a201e026b96173370081e1ac41758acce6e
Oryam <48219965+or-yam@users.noreply.github.com>
2020-09-01 16:19:14 +0300

    added register

--- database/tables.sql
+++ database/tables.sql
@@ -1,9 +1,12 @@
 USE cherryDb;
 
--- CREATE TABLE user(
---     id VARCHAR(30) PRIMARY KEY,
---     name VARCHAR(30),
---     email VARCHAR(30),
---     password VARCHAR(30),
---     img VARCHAR(30)
--- );
+CREATE TABLE user(
+    id int NOT NULL AUTO_INCREMENT ,
+    name VARCHAR(30),
+    email VARCHAR(30),
+    password VARCHAR(30),
+    img VARCHAR(30),
+    PRIMARY KEY(id)
+);
+
+-- DROP TABLE user;
