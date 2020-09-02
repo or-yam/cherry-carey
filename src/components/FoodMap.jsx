c6c0d2e567294831ab914fce09296ea95abc6bc7
Oryam <48219965+or-yam@users.noreply.github.com>
2020-09-02 15:24:00 +0300

    map init

--- src/components/FoodMap.jsx
+++ src/components/FoodMap.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+const mapStyles = {
+  width: '100%',
+  height: '100%',
+};
 
 const FoodMap = inject('user')(
   observer((props) => {
     console.log(props.user.name);
     return (
       <div>
-        <h1> hello {props.user.name}</h1>
-        <h2>
+        <div>
+          <h1> hello {props.user.name}</h1>
           <img src={props.user.img} alt="user" />
-        </h2>
-        <img
-          alt="map"
-          src="https://art-sheep.com/wp-content/uploads/2019/07/Taste-Atlas-artsheep.png"
+        </div>
+
+        <Map
+          google={props.google}
+          zoom={14}
+          style={mapStyles}
+          initialCenter={{
+            lat: -1.2884,
+            lng: 36.8233,
+          }}
         />
       </div>
     );
   })
 );
 
-export default FoodMap;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyAsAf8ZKSFF8-3xgRO1GOdhAsEEVxdc9a0',
+})(FoodMap);
