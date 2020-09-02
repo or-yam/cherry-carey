33bef6ba49f15ec173802f447aeacdc7a4c5e151
Alon <alonzager5@gmail.com>
2020-09-02 08:12:12 +0300

    login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import './App.css';
+import Login from './components/LogIn';
+import Register from './components/Register';
+import FoodMap from './components/FoodMap'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Router>
+      <div>
+        <Route path='/' exact render={() => <Login />} />
+        <Route path='/register' exact render={() => <Register />} />
+        <Route path='/foodMap' exact render={() => <FoodMap />} />
+        
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/FoodMap.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FoodMap() {
+    return (
+        <div>
+            <img src='https://art-sheep.com/wp-content/uploads/2019/07/Taste-Atlas-artsheep.png'></img>
+        </div>
+    )
+}
