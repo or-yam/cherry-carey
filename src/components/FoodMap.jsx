aa76afa96db18ebf1f7d3ced1ee9a4c0684b95ad
Oryam <48219965+or-yam@users.noreply.github.com>
2020-09-03 13:52:41 +0300

    post fixes

--- src/components/FoodMap.jsx
+++ src/components/FoodMap.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { inject, observer } from 'mobx-react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 import { Link } from 'react-router-dom';
@@ -8,14 +8,26 @@ const mapStyles = {
   height: '80%',
 };
 
-const FoodMap = inject('user')(
+const FoodMap = inject(
+  'user',
+  'posts'
+)(
   observer((props) => {
+    const { user, posts } = props;
+    useEffect(() => {
+      posts.getFoodPosts();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
       <div>
         <div>
-          <h1> hello {props.user.name}</h1>
-          <img src={props.user.img} alt="user" />
+          <h1> hello {user.name}</h1>
+          <img src={user.img} alt="user" />
           <Link to="/foodPost">Post</Link>
+          {posts.foodPosts.map((post) => (
+            <div>{post.mealName}</div>
+          ))}
         </div>
 
         <Map
