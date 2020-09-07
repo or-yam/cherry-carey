e39122abfaaefa0693b20329e1833b82946f20f8
Oryam <48219965+or-yam@users.noreply.github.com>
2020-09-07 21:00:22 +0300

    landing

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -6,24 +6,19 @@ import Logo from './Logo';
 
 const Landing = () => {
   return (
-    <div style={{ textAlign: 'center' }} className="landing">
+    <div className="landing">
       <Logo />
-      <h2 style={{ marginBottom: '0' }}>The Social Network For Food</h2>  
-      <div>
+      <h2>The Social Network For Food</h2>  
+      <div className="landing-img-container">
         <img
           alt="eating"
-          style={{ width: '40vh', height: '40vh', borderRadius: '50%' }}
           src="https://image.freepik.com/free-photo/friends-eating_23-2147680636.jpg"
         />
       </div>
-      <div>      
-        <Button
-          style={{ width: '40vh', textAlign: 'center' }}
-          variant="contained"
-          color="primary"
-        >
+      <div>
+        <Button variant="contained" color="primary">
           <Link to="/login">Let's start</Link>
-        </Button>    
+        </Button>
       </div>
     </div>
   );
