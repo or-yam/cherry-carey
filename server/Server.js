16f8b1d77a8712feef8c73756ecb20eddeaa6b01
Oryam <48219965+or-yam@users.noreply.github.com>
2020-09-10 10:10:05 +0300

    move to server from api.js

--- server/Server.js
+++ server/Server.js
@@ -5,13 +5,13 @@ const api = require('./Routes/Api');
 
 const PORT = process.env.PORT;
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', api);
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 app.listen(PORT, () => {
