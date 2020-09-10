27323dbbde5b2a90689ca6fc8ac93b807a4bc89d
Oryam <48219965+or-yam@users.noreply.github.com>
2020-09-10 09:44:17 +0300

    change script route

--- server/Server.js
+++ server/Server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const api = require('./Routes/api');
+const api = require('./Routes/Api');
 
 const PORT = 4000;
 const app = express();
